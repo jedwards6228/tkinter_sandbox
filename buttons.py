c0e16f861337ddba27ad8ae83fad510dbacd3ae8
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2022-01-10 15:29:05 -0800

    First (also a test, idk what I'm doing)

--- buttons.py
+++ buttons.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()
 
-my_button = Button(root, text="Click Me!", padx=50, pady=50)
+my_button = Button(root, text="Click Me!!", padx=50, pady=50)
 my_button.pack()
 
 
