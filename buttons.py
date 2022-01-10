520b1a126c975c83effae5d103bd5882d5286c95
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2022-01-10 15:37:07 -0800

    Experimenting with buttons

--- buttons.py
+++ buttons.py
@@ -4,11 +4,11 @@ root = Tk()
 
 
 def my_click():
-    my_label = Label(root, text="Look! I clicked a Button!")
+    my_label = Label(root, text="Look! I clicked a Button!", fg="red", bg="yellow")
     my_label.pack()
 
 
-my_button = Button(root, text="Click Me!!", padx=50, pady=50, command=my_click)
+my_button = Button(root, text="Click Me!!", padx=50, pady=50, command=my_click, fg="blue", bg="green")
 my_button.pack()
 
 
