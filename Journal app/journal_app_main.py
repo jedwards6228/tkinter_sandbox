7a948b9ff06cbfccaba4c82f0883bd26a48fb145
Jordan Edwards <jedwards6228@gmail.com>
2022-01-10 21:51:36 -0800

    Adding app opener and reorganizing files

---
+++ Journal app/journal_app_main.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+root = Tk()
+
+# Canvas
+# Pick Colors
+
+# Frame
+# Pick Colors
+
+# Function to load previous prompts
+
+# Button to add a prompt
+
+# Button to remove a prompt
+
+# Function to add a prompt
+
+# Function to remove a prompt
+
+# Button to save an entry
+
+# Research and pick a way to organize answers by date and prompt <<<<
+# Function to save an entry
+
+root.mainloop()
