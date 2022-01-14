06a50ef4885b586b8576304fae33e3832c0f88cc
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2022-01-14 10:57:03 -0800

    Working on 101 stuff

--- Tkinter101/images.py
+++ Tkinter101/images.py
@@ -1,10 +1,18 @@
 from tkinter import *
+from PIL import ImageTk,Image
 import os
 
 
 root = Tk()
 root.title('Tkinter 101 Images app')
 root.iconbitmap(os.getcwd() + '\Icons\chart_graphic_statistic_icon_208605.ico')
+
+my_img = ImageTk.PhotoImage(Image.open(os.getcwd() + '\Images\stock_photo.jpg'))
+my_label = Label(image=my_img)
+my_label.pack()
+
+button_quit = Button(root, text="Exit Program", command=root.quit)
+button_quit.pack()
 
 
 root.mainloop()
