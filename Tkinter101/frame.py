469caa5ce332b8ac961eeaff7909e1eb65ca1b7d
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2022-01-18 17:36:14 -0800

    Working on 101 stuff

---
+++ Tkinter101/frame.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from PIL import ImageTk,Image
+
+root = Tk()
+root.title("Frame project")
+
+my_frame = LabelFrame(root, text="Options", padx=100, pady=100)
+my_frame.pack(padx=10, pady=10)
+
+my_button1 = Button(my_frame, text="Add Topic")
+my_button1.grid(row=0, column=0, sticky=W+E)
+
+my_button2 = Button(my_frame, text="Remove Topic")
+my_button2.grid(row=1, column=0, sticky=W+E)
+
+my_button3 = Button(my_frame, text="Save")
+my_button3.grid(row=2, column=0, sticky=W+E)
+
+root.mainloop()
