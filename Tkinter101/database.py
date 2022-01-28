1bb9514eb577b010dad74fd249fb64107f2da3ea
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2022-01-27 22:21:36 -0800

    Working on 101 stuff

--- Tkinter101/database.py
+++ Tkinter101/database.py
@@ -2,6 +2,7 @@ from tkinter import *
 import sqlite3
 from pathlib import Path
 
+
 root = Tk()
 root.title('Database Project')
 root.geometry('400x400')
@@ -28,7 +29,7 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS addresses (
 
 # Data entry fields
 first_name = Entry(root, width=30)
-first_name.grid(row=0, column=1, padx=20)
+first_name.grid(row=0, column=1, padx=20, pady=(10, 0))
 
 last_name = Entry(root, width=30)
 last_name.grid(row=1, column=1)
@@ -47,7 +48,7 @@ zipcode.grid(row=5, column=1)
 
 # Create data labels
 first_name_label = Label(root, text="First Name")
-first_name_label.grid(row=0, column=0)
+first_name_label.grid(row=0, column=0, pady=(10, 0))
 
 last_name_label = Label(root, text="Last Name")
 last_name_label.grid(row=1, column=0)
@@ -63,6 +64,19 @@ state_label.grid(row=4, column=0)
 
 zipcode_label = Label(root, text="Zipcode")
 zipcode_label.grid(row=5, column=0)
+
+
+# Create function to delete a record
+def delete_record():
+    # Connect to the db and place the cursor
+    connect = sqlite3.connect(r'C:\Users\Jordan\PycharmProjects\Tkinter Sandbox\Tkinter101\Databases\address_book.db')
+    with connect:
+        cursor = connect.cursor()
+        # Delete a record
+        cursor.execute(f'DELETE from addresses WHERE oid = {delete_entry_id.get()}')
+        # Commit the change
+        connect.commit()
+    delete_entry_id.delete(0, END)
 
 
 # Create submit function
@@ -92,13 +106,9 @@ def submit_data():
     zipcode.delete(0, END)
 
 
-# Create a submit button
-submit_button = Button(root, text="Add Record To Database", command=submit_data)
-submit_button.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
-
-
 # Create query function
 def query_db():
+
     connect = sqlite3.connect(r'C:\Users\Jordan\PycharmProjects\Tkinter Sandbox\Tkinter101\Databases\address_book.db')
     with connect:
         cursor = connect.cursor()
@@ -108,12 +118,27 @@ def query_db():
         # Loop through results
         print_records = ''
         for entry in records:
-            for field in entry:
-                print_records += str(field) + "\n"
+            print_records += f"{str(entry[0])} {str(entry[1])}\t{str(entry[6])}\n"
 
         query_label = Label(root, text=print_records)
-        query_label.grid(row=8, column=0, columnspan=2)
+        query_label.grid(row=11, column=0, columnspan=2)
 
+
+# Create a delete button
+delete_button = Button(root, text='Delete Record', command=delete_record)
+delete_button.grid(row=8, column=0, columnspan=2,pady=10,padx=10, ipadx=129)
+
+# Create entry for which record to delete
+delete_entry_id = Entry(root, width=20, borderwidth=3)
+delete_entry_id.grid(row=9, column=1)
+
+# Create label for delete entry field
+delete_entry_id_label = Label(root, text="Enter ID to Delete")
+delete_entry_id_label.grid(row=9, column=0)
+
+# Create a submit button
+submit_button = Button(root, text="Add Record To Database", command=submit_data)
+submit_button.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
 
 # Create a Query Button
 query_button = Button(root, text="Show Records", command=query_db)
