e336f534d860870486edd98e6392faf0d2cf8ff0
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2022-01-29 15:43:34 -0800

    Created working login prototype which can now be repurposed for the journal app

--- Tkinter101/login.py
+++ Tkinter101/login.py
@@ -48,26 +48,45 @@ def register_user():
     credentials = [un_entry.get(), pw_entry.get()]
     # Reset fields but leave un up
     reset_fields(False, True, True)
+    # Basic pw check
+    if len(credentials[1]) < 4:
+        message_label['text'] = "Password is too short"
+        return
     # Check to see if they exist already
-    with connection:
-        cursor = connection.cursor()
-        sql = "SELECT * FROM user_credentials WHERE username = ? "
-        cursor.execute(sql, [credentials[0]])
-        checking = cursor.fetchall()
-        if len(checking) == 0:
-            # Create user
-            sql = "INSERT INTO user_credentials VALUES(?, ?)"
-            cursor.execute(sql, credentials)
-            connection.commit()
-            message_label['text'] = "User Created"
-        else:
-            message_label['text'] = "User name already exists"
+    stored_creds = find_credentials()
+    if len(stored_creds) == 0:
+        # Create user
+        sql = "INSERT INTO user_credentials VALUES(?, ?)"
+        cursor.execute(sql, credentials)
+        connection.commit()
+        message_label['text'] = "User Created"
+    else:
+        message_label['text'] = "User name already exists"
 
 
 def login_user():
-    return
+    # Get un and pw
+    entered_credentials = [(un_entry.get(), pw_entry.get())]
+    stored_credentials = find_credentials()
+    if entered_credentials == stored_credentials:
+        reset_fields(True, True, True)
+        message_label['text'] = "Credentials Accepted"
+    else:
+        reset_fields(False, True, True)
+        message_label['text'] = "Incorrect User Name or Password"
 
 
+# Function to find and store credentials for the un
+def find_credentials():
+    with connection:
+        cursor = connection.cursor()
+        sql = "SELECT * from user_credentials WHERE username = ?"
+        cursor.execute(sql, [un_entry.get()])
+        results = cursor.fetchall()
+    return results
+
+
+# Function for resetting fields on the page
 def reset_fields(username=False, password=False, message=False):
     if username:
         un_entry.delete(0, END)
