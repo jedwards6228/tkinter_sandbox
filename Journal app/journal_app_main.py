1bdebc7c51c2f51ad53cc9c2bd419bbb5f4d634a
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2022-01-29 16:05:33 -0800

    Created working login prototype which can now be repurposed for the journal app

--- Journal app/journal_app_main.py
+++ Journal app/journal_app_main.py
@@ -9,32 +9,11 @@ root.geometry("800x800+1300+150")
 canvas = Canvas(root, bg="#263D42")
 canvas.pack(fill="both", expand=True)
 
+# Login prompt
 # Create a Frame for un/pw prompt
 login_frame = Frame(canvas, bg="grey", width=500, height=300, highlightbackground="black", highlightthickness=1)
 login_frame.place(relx=.5, rely=.5, anchor="center")
 
-
-
-
-
-
-
-
-
-# Function to load previous prompts
-
-# Button to add a prompt
-
-# Button to remove a prompt
-
-# Function to add a prompt
-
-# Function to remove a prompt
-
-# Button to save an entry
-
-# Research and pick a way to organize answers by date and prompt <<<<
-# Function to save an entry
 
 if __name__ == '__main__':
     root.mainloop()
